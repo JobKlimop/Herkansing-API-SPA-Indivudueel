e16197aab87aee527bad08d07e7e2ba565f8b7ff
Unknown <thomas_641991@hotmail.com>
2018-02-15 12:29:35 +0100

    Change for heroku deploy

--- config/env/env.js
+++ config/env/env.js
@@ -3,7 +3,7 @@ const secretKey = 'UltraSecretTicketKey';
 let env = {
     webPort: process.env.PORT || 3000,
     dbHost: process.env.DB_HOST || 'localhost',
-    dbPort: process.env.dB_PORT || '',
+    dbPort: process.env.DB_PORT || '',
     dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
     dbDatabase: process.env.DB_DATABASE || 'ticketking'
